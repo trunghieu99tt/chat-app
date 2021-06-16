1eb74e93a03b2866df1821b49fdb077557fbba85
Nguyen Trung Hieu <trunghieu99tt@gmail.com>
2021-06-16 11:58:10 +0700

    debug

--- src/components/SocialLoginIcons/SocialLoginIcons.tsx
+++ src/components/SocialLoginIcons/SocialLoginIcons.tsx
@@ -43,8 +43,8 @@ const SocialLoginIcons = () => {
                     </button>
                 )}
                 buttonText="Login"
-                onSuccess={() => responseGoogle}
-                onFailure={() => responseGoogleFail}
+                onSuccess={(data) => responseGoogle(data)}
+                onFailure={() => responseGoogleFail()}
                 cookiePolicy={"single_host_origin"}
             />
             <div className="social-icons" onClick={showAlert}>
